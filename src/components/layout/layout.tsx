c6c56229eb34199213aa58282741c5f49f78dee6
awspi <206002328@qq.com>
2023-03-25 17:56:13 +0800

    修复: 🧩 修复首页overflow-y

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -6,7 +6,7 @@ import Header from "./header";
 export default function Layout({ children }: { children: React.ReactNode }) {
   const isMobile = useMobileTerminal()
   return (
-    <div className="h-screen fixed top-0 left-0 w-screen bg-white dark:bg-dark">
+    <div className="h-screen fixed top-0 left-0 w-screen bg-white  overflow-y-auto dark:bg-dark">
       <Header />
       <div className={classNames('m-auto', isMobile ? ' mx-auto w-full' : 'w-[750px]')}>
         <div className='mx-8'>
